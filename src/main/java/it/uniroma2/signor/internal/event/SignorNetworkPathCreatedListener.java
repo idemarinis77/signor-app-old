d05d8b159d5ca8e3ebd4a323db1fc8763767a023
Ilaria <ilaria.demarinis77@gmail.com>
2021-05-12 21:11:55 +0200

    deleted some files

--- src/main/java/it/uniroma2/signor/internal/event/SignorNetworkPathCreatedListener.java
+++
@@ -1,7 +0,0 @@
-package it.uniroma2.signor.internal.event;
-
-import org.cytoscape.event.CyListener;
-
-public interface SignorNetworkPathCreatedListener extends CyListener {
-    void handleEvent(SignorNetworkCreatedEvent event);
-}
